903d76022b8a11d8133a0edbcbb78e7423b68646
Avinash Reddy <avinashr175@gmail.com>
2021-03-15 14:30:48 -0700

    Add files via upload

---
+++ comparison.m
@@ -0,0 +1,60 @@
+clc;
+clear;
+base_algo = [];
+max_algo = [];
+for EV_count=[10 20 30 40 50]
+    base_avg = 0;
+    max_avg = 0;
+    for itr=1:3
+        T = 24;
+        cs_del_capacity = 100 ; %R
+        reg_price = rand(1,T); %at
+        elec_price = [0.04921741;
+        0.06635104;
+        0.05469124;
+        0.04867549;
+        0.04361576;
+        0.03636411;
+        0.03338169;
+        0.03001837;
+        0.02966395;
+        0.02925406;
+        0.02952074;
+        0.03153413;
+        0.03627351;
+        0.05047265;
+        0.03441526;
+        0.02829829;
+        0.02567869;
+        0.02133562;
+        0.01965296;
+        0.01343761;
+        0.01209589;
+        0.01430528;
+        0.0197348;
+        0.03304057]; %gt
+        %elec_price = rand(1,T);
+        efficiency = 0.8*ones(1,EV_count); %Ei
+        cost_upper_bound = 11*EV_count; %B
+
+
+        start_time = 2*randn(1,EV_count)+6; %si
+        end_time = 2*randn(1,EV_count)+19; %fi
+        soc_initial = 0.6*rand(1,EV_count)+0.3; %ei
+        soc_final = 0.9*ones(1,EV_count); %ei_dash
+        upper_charging_limit = 4.4*ones(1,EV_count); % in kW
+        lower_charging_limit = 3.3*ones(1,EV_count); % in kW
+        M = 0.05;
+        battery_capacities = 16.5*ones(1,EV_count); % in kWh (Ci)
+        [charging_rate,soc_ev,revenue,baseline_revenue] = max_revenue(EV_count,T,cs_del_capacity,reg_price,elec_price,efficiency,cost_upper_bound,start_time,end_time,soc_initial,soc_final,upper_charging_limit,lower_charging_limit,M,battery_capacities);
+        base_avg = base_avg + baseline_revenue;
+        max_avg = max_avg + revenue;
+    end
+    base_algo(end+1) = base_avg/3;
+    max_algo(end+1) = max_avg/3;
+end
+bar([10 20 30 40 50],[base_algo;max_algo])
+legend('Baseline','Revenue maximization algo','Location','Northwest')
+xlabel('Number of EVs')
+ylabel('Charging station operator revenue (in $)')
+title('Revenue gain comparison')
---
+++ max_revenue.m
@@ -0,0 +1,117 @@
+function [y,soc_ev,obj,baseline_revenue] = max_revenue(EV_count,T,cs_del_capacity,reg_price,elec_price,efficiency,cost_upper_bound,start_time,end_time,soc_initial,soc_final,upper_charging_limit,lower_charging_limit,M,battery_capacities)
+h = zeros(EV_count,T);
+for i=1:EV_count
+    for t=1:T
+        if(t>floor(start_time(i)) && t<floor(end_time(i)) )
+            h(i,t) = 1;
+        elseif(t==floor(start_time(i)) && start_time(i)==floor(start_time(i)) )
+            h(i,t) = 1;
+        elseif(t==floor(start_time(i)) && start_time(i)~=floor(start_time(i)))
+            h(i,t) = ceil(start_time(i)) - start_time(i);
+        elseif(t==floor(end_time(i)) && end_time(i)~=floor(end_time(i)))
+            h(i,t) = end_time(i) - floor(end_time(i));
+        else
+            h(i,t) = 0;
+        end
+    end
+end
+
+cvx_begin
+    variable charging_rate(EV_count,T);
+    variable regulation_capacity(EV_count,T);
+    variable soc_ev(EV_count,T);
+    variable u(EV_count,T);
+    variable d(EV_count,T);
+    variable delta binary;
+    
+    obj = 0;
+    for t=1:T
+        x = 0;
+        for i=1:EV_count
+            obj = obj + M*charging_rate(i,t)*h(i,t);
+            x = x+regulation_capacity(i,t);
+        end
+        %obj = obj + reg_price(t)*x;
+    end
+    cost_to_cust = 0;
+    maximize(obj)
+    subject to
+    for t=1:T
+        
+        for i=1:EV_count
+            charging_rate(i,t)>=0;
+            charging_rate(i,t)<=upper_charging_limit(i);
+            cost_to_cust = cost_to_cust + charging_rate(i,t)*h(i,t);
+            if(h(i,t)==1)
+                u(i,t) == charging_rate(i,t)-lower_charging_limit(i);
+                %d(i,t) == min((soc_final(i)-soc_ev(i,t))*battery_capacities(i)/efficiency(i),upper_charging_limit(i))-charging_rate(i,t);
+                d(i,t) == upper_charging_limit(i)-charging_rate(i,t); 
+            else
+                u(i,t) == 0;
+                d(i,t) == 0;
+            end
+            regulation_capacity(i,t) == u(i,t)+d(i,t);
+            %soc_ev(i,t) <= soc_final(i);
+            if(t==floor(start_time(i)))
+                soc_ev(i,t) == soc_initial(i);
+            elseif(t<floor(start_time(i)))
+                soc_ev(i,t)==0;
+                %charging_rate(i,t)==0;
+            else
+                if(t>1)
+                    soc_ev(i,t) == soc_ev(i,t-1)+efficiency(i)*h(i,t-1)*charging_rate(i,t-1)/battery_capacities(i);
+                    
+                end
+                
+%                 soc_ev(i,t)>=soc_final(i)-1.2*(1-delta);
+%                 soc_ev(i,t)<=soc_final(i)+1.2*delta;
+%                 -1.2*(1-delta)<=h(i,t)<=1.2*delta;
+            
+            end
+            soc_ev(i,t) <= soc_final(i);
+        end
+        cost_to_cust = cost_to_cust*(M+elec_price(t));
+        sum(charging_rate(:,t).*h(:,t)) <= cs_del_capacity;
+    end
+    cost_to_cust<=cost_upper_bound;
+    
+        
+cvx_end
+
+y = charging_rate.*sign(h);
+% 
+% 
+% %%%%%%%% unregulated charging scheme (first come first serve)
+charging_rates2 = zeros(EV_count,T);
+charging_lim = 4.4;
+soc_ev2 = zeros(EV_count,T);
+[out,idx] = sort(start_time);
+for t=1:T
+    c = 0;
+    for i=idx
+        if(c>=cs_del_capacity)
+            break
+        end
+        
+        if(t==floor(start_time(i)))
+            soc_ev2(i,t) = soc_initial(i);
+            charging_rates2(i,t) = min(charging_lim,cs_del_capacity-c);
+            c = c + charging_rates2(i,t);
+        else
+            if(t>1 && t>floor(start_time(i)))
+                soc_ev2(i,t) = min(soc_final(i),soc_ev2(i,t-1)+efficiency(i)*h(i,t-1)*charging_rates2(i,t-1)/battery_capacities(i));
+                if(soc_ev2(i,t)>=soc_final(i))
+                    charging_rates2(i,t)=0;
+                    continue
+                end
+                charging_rates2(i,t) = min(charging_lim,cs_del_capacity-c);
+                c = c + charging_rates2(i,t);
+            end
+        end
+    end
+end
+
+baseline_revenue = sum(sum(M*charging_rates2.*elec_price'));
+end
+
+
